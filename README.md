eb19779fb671d166446fb684e386cc67beb1d751
Greg Buckner <gbuckner23@gmail.com>
2014-04-05 12:48:57 -0700

    added test data script to seed file and created a markdown readme

---
+++ README.md
@@ -0,0 +1,13 @@
+#ATM - the cache machine
+
+Simple rails app to mess around with caching.
+
+
+ATM includes two model, users and matches. A match is represents a meeting between two users.
+
+Getting Started
+---
+
+1. ```rake db:migrate```
+
+2. ```rake db:seed``` for some tasty test data.
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+1000.times { User.create(name: Faker::Name.name) }
+ids = User.ids
+
+10000.times do
+  match = Match.new(host_id: ids.sample, activity: Faker::Company.bs)
+  match.guest_id = ids.sample until match.guest_id && match.guest_id != match.host_id
+  match.save
+end
