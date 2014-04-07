0876ff7d30962ff70124445b3c14edc8855b5851
Greg Buckner <gbuckner23@gmail.com>
2014-04-07 16:19:40 -0700

    updated gemfile and gem lock and database to use pg locally

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140405173130) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "matches", force: true do |t|
     t.integer  "host_id"
     t.integer  "guest_id"
