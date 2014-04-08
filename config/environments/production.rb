f2d403f6ed50d3868e483e138ed3289f34d064d0
Greg Buckner <gbuckner23@gmail.com>
2014-04-07 17:05:48 -0700

    moved config.cache_store to environment files

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,7 +52,7 @@ Atm::Application.configure do
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
 
   # Use a different cache store in production.
-  # config.cache_store = :mem_cache_store
+  config.cache_store = :redis_store, ENV['REDIS_PROVIDER']
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = "http://assets.example.com"
